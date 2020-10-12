e50f7536cf2d8109826834422d47e6cafd5bb59b
nadia-rais <nadia.rais@laplateforme.io>
2020-10-12 11:13:56 +0200

    initialisation fichiers

---
+++ connexion.php
@@ -0,0 +1,29 @@
+<?php $page_selected = 'connexion'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>chat - connexion</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes"/>
+    <link rel="shortcut icon" type="image/x-icon" href="#">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+</head>
+<body>
+<header>
+    <?php
+    include("includes/header.php");
+   ?>
+</header>
+<main>
+</main>
+<footer>
+    <?php
+    include("includes/footer.php") ?>
+</footer>
+</body>
+</html>
