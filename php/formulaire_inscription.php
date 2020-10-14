cffde93c7566e58981c37e3f7839873caf055087
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2020-10-14 16:29:46 +0200

    page de traitement formulaire inscription php

---
+++ php/formulaire_inscription.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+require '../class/Connexion.php';
+require '../class/User.php';
+
+$bdd = new Connexion("localhost","root","chat","");
+
+$user = new User($bdd);
+
+
+//ENREGISTREMENT BDD
+if (isset($_POST['valider'])) {
+    
+    $login = $_POST['login'];
+    $password1 = $_POST['password'];
+    $password2 = $_POST['password2'];
+    $mail = $_POST['mail'];
+    $mail2 = $_POST['mail2'];
+    
+
+    $user->inscription($mail,$mail2,$login,$password1,$password2);
+}
+
+
+//VERIFICATION LOGIN AVEC AJAX
+if (isset($_POST['pseudo'])) {
+    $resultat = $user->getlogin($_POST['pseudo']);
+
+    if ($resultat > 0){
+        echo "1";
+    }
+    else {
+        echo "0";
+    } 
+}
+
+
+?>
