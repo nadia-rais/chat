38599566f0fa24f59371da629e8ad94a36070c8b
nadia-rais <nadia.rais@laplateforme.io>
2020-10-14 16:27:20 +0200

    démarrage chat partie php
    class db test + channel class + form chat

--- chat.php
+++ chat.php
@@ -1,5 +1,4 @@
 <?php $page_selected = 'chat'; ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,9 +16,45 @@
 <header>
     <?php
     include("includes/header.php");
+    $title_channel = $channel->channel();
+    //var_dump($title_channel);
+    $list_messages = $channel->messages(1);
+    //var_dump($list_messages);
    ?>
 </header>
 <main>
+    <!-- Page Layout here -->
+    <div class="row">
+        <!-- Lateral nav pour afficher les infos de l'utilisateur connecté + la liste des channels -->
+        <div class="col s3 teal lighten-2">
+            <ul>
+                <li>
+                    <div class="user-view">
+                        <div class="background center">
+                            <a href="#user_picture"><img class="circle" src="img/default_profile.png" width="70"></a>
+                        </div>
+                        <a href="#user_name"><span class="white-text name">John Doe</span></a>
+                        <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+                    </div>
+                </li>
+                <li><a href="#!"><i class="material-icons">cloud</i>CHANNELS</a></li>
+                <?php  var_dump($title_channel); ?>
+                <li><a href="#!">Second Link</a></li>
+                <li><div class="divider"></div></li>
+                <li><a class="subheader">Subheader</a></li>
+                <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+            </ul>
+        </div>
+
+        <div class="col s9">
+            <!-- AFFICHAGE DES MESSAGES + FORM POUR POSTER  -->
+            <form id="chat" action="traitement_php/message.php" method="POST">
+                <input type="text" name="message" placeholder="say-hi!" size="144" required>
+                <button type="submit"><i class="material-icons right">near_me</i></button>
+            </form>
+        </div>
+
+    </div>
 </main>
 <footer>
     <?php
---
+++ class/channel_class.php
@@ -0,0 +1,66 @@
+<?php
+
+class Channel{
+
+public $db;
+
+public function __construct($db)
+{
+	$this->db = $db;
+}
+
+// test méthode pour récupérer la liste des channel cf admin
+public function channel()
+    {
+        $connexion = $this->db->connectDb();
+        $q = $connexion->prepare("SELECT * FROM canaux");
+        $q->execute();
+		$channel = $q->fetchAll();
+
+		foreach ($channel as $channels){
+			$name_chan = $channels["name_channel"];
+			$id_chan = $channels["id_channel"];
+			//echo '<a href="channel.php?id='.$id_chan.'">' .$name_chan.'</a>';
+			};
+		
+		return $channel;       
+	}
+
+
+// SELECT CHAN + TOUS LES MESSAGES ASSOCIÉS AU CHANNEL // cf admin
+public function chat_channel($id_chan, $id_chat)
+    {
+        $connexion = $this->db->connectDb();
+        $q1 = $connexion->prepare("SELECT * FROM canaux INNER JOIN messages ON canaux.id = messages.id ");
+        $q1->execute();
+		$chat_by_channel = $q1->fetchAll();
+
+		foreach ($chat_by_channel as $chat_by_channels){
+			$name_channel = $chat_by_channels['name_channel'];
+			$id_channel = $chat_by_channels["id_channel"];
+			$id_message = $chat_by_channels["id_messages"];
+			$content_message = $chat_by_channels["content"];
+			echo '<li><a href="channel.php?id='.$id_channel.'&channel_name='.$name_channel.'/messages">'.$name_channel.'</a></li>';
+		}
+
+		return chat_channel;       
+	}
+
+
+// SELECT LES MESSAGES + CHANNEL + L'UTILISATEUR ASSOCIÉ AU MESSAGE 
+public function messages($id_chan) // on passe en paramètre l'id du channel
+    {
+        $connexion = $this->db->connectDb();
+        $q = $connexion->prepare("SELECT * 
+								  FROM messages as M
+								  INNER JOIN canaux as C ON M.id_channel = C.id_channel
+								  INNER JOIN utilisateurs as U ON M.id_utilisateur = U.id
+								  WHERE M.id_channel = $id_chan order by M.created_in DESC");
+        $q->execute();
+		$messages = $q->fetchAll();
+		
+		return $messages;       
+	}
+}
+?>
+
