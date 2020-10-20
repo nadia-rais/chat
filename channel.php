3051fddbc0bf7cbd944e3e2319de00dac3263bea
nadia-rais <nadia.rais@laplateforme.io>
2020-10-20 15:47:56 +0200

    update channel

--- channel.php
+++ channel.php
@@ -19,6 +19,7 @@
 <body>
 <header>
     <?php
+    session_start();
     include("includes/header.php");
     $id_page = $_GET['id'];
     $last_mess = $channel->messages($id_page); // methode de la class channel pour récup tous les messages + channel + infos users 
@@ -31,9 +32,10 @@
     </nav>
 </header>
 <main data-id_page="<?= $id_page;?>" id="main-channel">
-    <div class="row">
+    <?php if(isset ($_SESSION['user'])){ ?>
+    <div class="row row-chat">
         <!-- Lateral nav pour afficher les infos de l'utilisateur connecté + la liste des channels -->
-        <div class="col s3 profile-side">
+        <div class="col l3 s12 profile-side">
             <ul>
                 <li>
                     <div class="user-view">
@@ -46,23 +48,18 @@
                         
                     </div>
                 </li>
-                <li><div class="divider"></div></li>
                 <li id="title-category"> # CHANNELS</li>
+                <li><div class="divider"></div></li>
                 <div class="collection">
                     <?php foreach ($title_channel as $channels){ ?>
                         <a class="collection-item transparent" href="channel.php?id=<?= $channels['id_channel'] ?>"># <?= $channels['name_channel'] ?></a>
                     <?php }; ?>
                 </div>
                 <li><div class="divider"></div></li>
-                <li id="disconnect-channel">
-                    <form action="index.php" method="post">
-                        <input class="btn waves-effect waves-light black" name="deco" value="SE DECONNECTER" type="submit"/>
-                    </form>
-                </li>
             </ul>
         </div>
 
-        <div class="col s9">
+        <div class="col l9 s12">
             <!-- AFFICHAGE DES MESSAGES + FORM POUR POSTER  -->
             <section id="messages">
                 <?php //var_dump($last_mess); ?>
@@ -82,8 +79,15 @@
             <p class="error" style="color:red"></p>
         </div>
     </div>
+    <?php } else {?>
+        <div class="container">
+            <div class="row">
+                <p class="center-align white-text" >connectez vous pour accèder à cette page </p>
+            </div>
+        </div>
+    <?php }; ?>
+    <script type="text/javascript" src="js/chat.js"></script>
 </main>
-<script type="text/javascript" src="js/chat.js"></script>
 <footer>
     <?php
     include("includes/footer.php") ?>
